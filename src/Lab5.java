c522fd4e5b95f15dc5be62bfdb381c523a9a6ed5
aj0strow <alexander.ostrow@gmail.com>
2013-10-12 13:04:49 -0400

    first part done

--- src/Lab5.java
+++ src/Lab5.java
@@ -1,4 +1,6 @@
 import lejos.nxt.*;
+import lejos.nxt.ColorSensor;
+import lejos.util.*;
 
 public class Lab5 {
 
@@ -6,6 +8,31 @@ public class Lab5 {
 		Button.ESCAPE.addButtonListener(new ExitListener());
 		
 		Button.waitForAnyPress();
+		
+		SensorPort s3 = SensorPort.S3;
+		final ColorSensor colorSensor = new ColorSensor(s3);
+				
+		Timer timer = new Timer(300, new TimerListener() {
+			public void timedOut() {
+				LCD.clear();				
+				String descriptor = detection(colorSensor.getColor());
+				if (descriptor.length() > 0) {
+					LCD.drawString("Object Detected", 0, 0);
+					LCD.drawString(descriptor, 0, 1);
+				}
+			}
+					
+			public String detection(ColorSensor.Color color) {
+				int red = color.getRed(), blue = color.getBlue();
+				String descriptor = "";
+				if (red > 10 && blue > 10) {
+					descriptor = red > blue ? "wood" : "styrofoam";
+				}
+				return descriptor;
+			}
+		});
+		
+		timer.start();
 		
 		// lab code here
 		
