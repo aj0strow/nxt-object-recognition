226e4f1409a58a2b4079b7523472fd04aa144167
aj0strow <alexander.ostrow@gmail.com>
2013-10-12 14:22:01 -0400

    basic odometry working

---
+++ src/Odometer.java
@@ -0,0 +1,83 @@
+import lejos.util.Timer;
+import lejos.util.TimerListener;
+import lejos.nxt.LCD;
+
+public class Odometer implements TimerListener {
+	private static final int PERIOD = 10;
+	private Timer timer;
+	
+	private Position position;
+	private Object lock;
+	
+	private Robot robot;
+	
+	double displacement = 0.0, heading = 0.0;
+
+	public Odometer(Robot robot) {
+		this.position = new Position(0.0, 0.0, 0.0);
+		this.lock = new Object();
+		
+		this.robot = robot;
+		
+		(new Timer(PERIOD, this)).start();
+	}
+	
+	public void timedOut() {
+		double newDisplacement = robot.getDisplacement();
+		double newHeading = robot.getHeading();
+		
+		incrPosition(newDisplacement - displacement, heading - newHeading);
+		
+		this.displacement = newDisplacement;
+		this.heading = newHeading;
+		
+		displayPosition();
+	}
+	
+	public Position getPosition() {
+		Position position = null;
+		synchronized (lock) {
+			position = this.position.clone();
+		}
+		return position;
+	}
+	
+	public void setPosition(Position position) {
+		synchronized (lock) {
+			this.position = position.clone();
+		}
+	}
+	
+	public void incrPosition(double centimeters, double radians) {
+		double theta = getTheta();
+		
+		double dx = centimeters * Math.cos(theta);
+		double dy = centimeters * Math.sin(theta);
+		
+		incrPosition(dx, dy, radians);
+	}
+	
+	public void incrPosition(double dx, double dy, double dtheta) {
+		synchronized (lock) {
+			this.position.x += dx;
+			this.position.y += dy;
+			this.position.theta += dtheta;
+		}
+	}
+	
+	// 0 <= theta < 2π
+	public double getTheta() {
+		double result;
+		synchronized (lock) { result = position.theta; }
+		return result;
+	}
+	
+	public void displayPosition() {
+		Position p = getPosition();
+		LCD.clear();
+		LCD.drawString("x: " + p.x, 0, 0);
+		LCD.drawString("y: " + p.y, 0, 1);
+		LCD.drawString("t: " + p.theta, 0, 2);
+	}
+
+}
---
+++ src/Point.java
@@ -0,0 +1,14 @@
+/*
+*  Point is the cartesian coordinates of a place on
+*  the board (x, y).
+*/
+
+public class Point {
+	public double x;
+	public double y;
+	
+	public Point(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+}
